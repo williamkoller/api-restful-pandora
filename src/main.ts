648338c8d25efaae2b0098cd8aa7d720556f6946
william koller <wkoller25@gmail.com>
2021-07-11 00:15:22 -0300

    chore: add validation pipe

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/modules/app/app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
-async function bootstrap() {
+const key = 'fetch';
+
+global[key] = require('node-fetch');
+
+declare const module: any;
+
+async function bootstrap(): Promise<void> {
   const logger = new Logger('Main');
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT, () => logger.log(`App running 🔥`));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
