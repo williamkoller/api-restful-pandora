c3eab8ff864b9b37006605c988c207f2e757a554
william koller <wkoller25@gmail.com>
2021-07-11 00:41:17 -0300

    feat: add users module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,7 +1,13 @@
-import { Module } from '@nestjs/common';
+import { configService } from '@/infra/typeorm/config/config.service';
+import { forwardRef, Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from '@/modules/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()[0]),
+    forwardRef(() => UsersModule),
+  ],
   controllers: [],
   providers: [],
 })
