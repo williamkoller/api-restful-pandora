fe92487e05a04ab9adc9f320ba3a69563012dcde
william koller <wkoller25@gmail.com>
2021-07-13 23:19:22 -0300

    chore: adjusted path absolute

--- src/modules/core/core.module.ts
+++ src/modules/core/core.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { LoggingInterceptor } from './interceptors/logging.interceptor';
-import { TransformInterceptor } from './interceptors/transform.interceptor';
+import { LoggingInterceptor } from '@/modules/core/interceptors/logging.interceptor';
+import { TransformInterceptor } from '@/modules/core/interceptors/transform.interceptor';
 
 @Module({
   providers: [
