2569be0b863a271d2d5177c86062999d1dfb8454
william koller <wkoller25@gmail.com>
2021-08-10 16:08:47 -0300

    refactor: alter migrations alter table users

--- src/infra/db/migrations/1628604915760-AlterTableUsers.ts
+++ src/infra/db/migrations/1628604915760-AlterTableUsers.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class AlterTableUsers1628604915760 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE "users" ADD COLUMN "last_logged" date;`,
+      `ALTER TABLE "users" ADD COLUMN "last_logged" TIMESTAMP WITH TIME ZONE;`,
     );
   }
 
