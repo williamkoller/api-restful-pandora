a7aac129624ad72fcdcb34dc3f1dd4deebb8fccb
william koller <wkoller25@gmail.com>
2021-08-20 15:55:44 -0300

    feat: add SentryInterceptor

--- src/main.ts
+++ src/main.ts
@@ -6,17 +6,23 @@ import { ModelNotFoundException } from '@/common/filters/model-not-found.excepti
 import { TimeoutInterceptor } from '@/common/interceptors/timeout.interceptor';
 import { swaggerConfig } from '@/docs/swagger-config';
 import { HttpExceptionFilter } from '@/common/filters/http-exception.filter';
+import { configService } from '@/infra/db/config/config.service';
+import { SentryInterceptor } from '@/modules/core/interceptors/sentry.intercepor';
+import * as Sentry from '@sentry/node';
 
 async function bootstrap(): Promise<void> {
   const logger = new Logger('Main');
-
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  const { environment, dsn } = configService.getSentryConfig();
+
+  Sentry.init({ environment, dsn });
 
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useGlobalFilters(new ModelNotFoundException(), new HttpExceptionFilter());
-  app.useGlobalInterceptors(new TimeoutInterceptor());
+  app.useGlobalInterceptors(new TimeoutInterceptor(), new SentryInterceptor());
 
   swaggerConfig(app);
 
