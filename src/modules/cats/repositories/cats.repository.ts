9510100aaf755e18ff24237025183986e720e559
william koller <wkoller25@gmail.com>
2021-08-22 22:52:50 -0300

    feat: add findByID, findByName and findCatAndCount in cats repository

--- src/modules/cats/repositories/cats.repository.ts
+++ src/modules/cats/repositories/cats.repository.ts
@@ -4,8 +4,46 @@ import { CreateCatDto } from '../dtos/create-cat/create-cat.dto';
 
 @EntityRepository(Cat)
 export class CatsRepository extends Repository<Cat> {
+  /**
+   * @param {CreateCatDto} createCatDto
+   * @return {*}  {Promise<Cat>}
+   * @memberof CatsRepository
+   */
   public async add(createCatDto: CreateCatDto): Promise<Cat> {
     const createdCat = this.create(createCatDto);
     return await this.save(createdCat);
+  }
+
+  /**
+   * @param {string} id
+   * @return {*}  {Promise<Cat>}
+   * @memberof CatsRepository
+   */
+  public async findById(id: string): Promise<Cat> {
+    return await this.findOne({ where: { id } });
+  }
+
+  /**
+   * @param {string} name
+   * @return {*}  {Promise<Cat>}
+   * @memberof CatsRepository
+   */
+  public async finByName(name: string): Promise<Cat> {
+    return await this.createQueryBuilder('cats')
+      .where('(cats.name ILIKE :name)', { name: `%${name}%` })
+      .getOne();
+  }
+
+  /**
+   * @param {number} offset
+   * @param {number} limit
+   * @return {*}  {Promise<[Cat[], number]>}
+   * @memberof CatsRepository
+   */
+  public async findCatAndCount(
+    offset: number,
+    limit: number,
+  ): Promise<[Cat[], number]> {
+    return await this.findAndCount({ skip: offset, take: limit });
   }
 }
