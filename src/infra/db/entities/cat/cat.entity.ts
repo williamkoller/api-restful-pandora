f7d22c44ca8ec3b9ea646d455595fae42f57ffaa
william koller <wkoller25@gmail.com>
2021-10-11 20:34:09 -0300

    refactor: alter typing in cat entity

--- src/infra/db/entities/cat/cat.entity.ts
+++ src/infra/db/entities/cat/cat.entity.ts
@@ -1,10 +1,16 @@
-import { Column } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { BaseEntity } from '@/infra/db/entities/base-entity/base-entity';
 
+@Entity('cats')
 export class Cat extends BaseEntity {
   @Column({ type: 'varchar', nullable: false })
   name: string;
 
-  @Column({ type: 'date', nullable: false })
-  age: Date;
+  @Column({ type: 'varchar', nullable: false })
+  age: string;
+
+  constructor(partial: Partial<Cat>) {
+    super();
+    Object.assign(this, partial);
+  }
 }
